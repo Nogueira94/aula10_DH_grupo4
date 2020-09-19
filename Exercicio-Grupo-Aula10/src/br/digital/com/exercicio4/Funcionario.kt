381687e72942b98d4c460ec728b3227dd75f38ae
Damorei <danilosilver@gmail.com>
2020-09-18 22:02:31 -0300

    correção override equals

--- Exercicio-Grupo-Aula10/src/br/digital/com/exercicio4/Funcionario.kt
+++ Exercicio-Grupo-Aula10/src/br/digital/com/exercicio4/Funcionario.kt
@@ -1,9 +1,11 @@
-package Equals
+package br.digital.com.exercicio4
 
 data class Funcionario(var nome :String, var numeroDeRegistro: Int) {
-    override fun equals(other: Any?): Boolean {
-        if (numeroDeRegistro != other) return false
-        return true
-    }
+   override fun equals(other: Any?): Boolean {
+       return if (other is Funcionario) {
+           other.numeroDeRegistro == this.numeroDeRegistro
+   }
+        else false
+   }
 
 }
